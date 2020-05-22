3f4297e3e128a907da87090219feae8456a82e23
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-05-22 13:39:27 -0400

    Handle images on backend

--- drawful-backend/app/controllers/drawings_controller.rb
+++ drawful-backend/app/controllers/drawings_controller.rb
@@ -1,6 +1,25 @@
+require 'securerandom'
+
 class DrawingsController < ApplicationController
   def index
     drawings = Drawing.all
     render json: drawings, only: [:file]
+  end
+
+  def create
+    puts params[:image]
+    puts params[:user]
+    puts params[:prompt]
+    id = SecureRandom.uuid
+    # https://stackoverflow.com/questions/21707595
+    File.open("#{Rails.root}/../frontend/assets/#{id}.png", 'wb') do |file|
+      file.write(params[:image].read)
+    end
+    # TODO: maybe check that the file write succeeded?
+    d = Drawing.new
+    d.user = User.first # TODO: attach to params[:user], when implemented
+    # TODO: d.prompt should be assigned
+    d.file = "#{id}.png"
+    d.save!
   end
 end
