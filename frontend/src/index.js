f143cd5c18d5a2d76ade24d3d5ff89779fa45611
theluispestana <theluispestana@gmail.com>
2020-05-22 12:11:15 -0400

    fetching and rendering images

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,24 @@
-console.log('hello world')
+function displayImage() {
+  const drawingsUrl = "http://localhost:3000/drawings/"
+  const imageDiv = document.getElementById("show-image")
+
+  function fetchImage() {
+    fetch(drawingsUrl)
+      .then(res => res.json())
+      .then(renderImage)
+  }
+
+  function renderImage(image) {
+    imageDiv.innerHTML = ''
+
+    const imageElement = document.createElement("img")
+    const randomFile = image[Math.floor(Math.random() * image.length)].file
+    imageElement.src = randomFile
+
+    imageDiv.appendChild(imageElement)
+  }
+
+  fetchImage()
+}
+
+displayImage()
