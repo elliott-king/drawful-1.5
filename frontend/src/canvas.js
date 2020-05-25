55411c6aa8da1d4cd67bc89aeb4345160fb3759f
theluispestana <theluispestana@gmail.com>
2020-05-24 21:05:57 -0400

    used css grids to improve element positions

--- frontend/src/canvas.js
+++ frontend/src/canvas.js
@@ -1,81 +1,79 @@
 function addUploadButton(div) {
-
-  const btn = document.createElement('button')
-  btn.setAttribute('id', 'image-upload-button')
-  btn.textContent = "Upload"
-  btn.addEventListener('click', (event) => {
-    const canvas = document.getElementById('user-image-canvas')
-    const prompt = document.getElementById("prompt")
-    const dataUrl = canvas.toDataURL("image/png;base64;")
+  const btn = document.createElement("button");
+  btn.setAttribute("id", "image-upload-button");
+  btn.textContent = "Upload";
+  btn.addEventListener("click", (event) => {
+    const canvas = document.getElementById("user-image-canvas");
+    const prompt = document.getElementById("prompt");
+    const dataUrl = canvas.toDataURL("image/png;base64;");
 
     // https://stackoverflow.com/questions/21707595
-    const file = dataURLtoBlob(dataUrl)
-    let fd = new FormData()
+    const file = dataURLtoBlob(dataUrl);
+    let fd = new FormData();
     fd.append("image", file);
-    fd.append("prompt", prompt.dataset.id)
-    fd.append("user", "TODO: insert username here")
+    fd.append("prompt", prompt.dataset.id);
+    fd.append("user", "TODO: insert username here");
     // console.dir(`fd form promp id = ${prompt.dataset.id}`)
 
     // And send it
-    const url = "http://localhost:3000/drawings"
+    const url = "http://localhost:3000/drawings";
     fetch(url, {
       method: "POST",
       // Should be good by just dropping it in the body
       // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-      body: fd
-    }).then(res => {
+      body: fd,
+    }).then((res) => {
       // console.log('image upload response', res)
       // return res.json()
       // remove canvas elements and render image
-      displayImage()
-    })
-
-  })
+      displayImage();
+    });
+  });
 
   // Convert dataURL to Blob object
   function dataURLtoBlob(dataURL) {
-
     // Decode the dataURL
-    var binary = atob(dataURL.split(',')[1]);
+    var binary = atob(dataURL.split(",")[1]);
 
     // Create 8-bit unsigned array
     var array = [];
-    for(var i = 0; i < binary.length; i++) {
+    for (var i = 0; i < binary.length; i++) {
       array.push(binary.charCodeAt(i));
     }
 
     // Return our Blob object
-    return new Blob([new Uint8Array(array)], {type: 'image/png'});
+    return new Blob([new Uint8Array(array)], { type: "image/png" });
   }
 
-  div.appendChild(btn)
+  div.appendChild(btn);
 }
 
 function displayPrompt(div) {
-  const prompsUrl = "http://localhost:3000/prompts"
+  const prompsUrl = "http://localhost:3000/prompts";
   fetch(prompsUrl)
-    .then(res => res.json())
-    .then(renderPrompt)
+    .then((res) => res.json())
+    .then(renderPrompt);
 
   function renderPrompt(prompts) {
-    const randomPrompt = prompts[Math.floor(Math.random() * prompts.length)]
-    const promptElement = document.createElement("p")
-    promptElement.dataset.id = randomPrompt.id
+    const randomPrompt = prompts[Math.floor(Math.random() * prompts.length)];
+    const promptElement = document.createElement("p");
+    promptElement.dataset.id = randomPrompt.id;
     // console.log(promptElement.dataset.id)
-    promptElement.id = "prompt"
-    promptElement.textContent = randomPrompt.title
+    promptElement.id = "prompt";
+    promptElement.textContent = randomPrompt.title;
 
-    div.prepend(promptElement)
+    div.prepend(promptElement);
   }
 }
 
 // Taken from https://stackoverflow.com/questions/2368784/
 function createCanvas(div) {
-
+  const container = document.getElementById("container");
+  container.className = "sp-draw";
   // create canvas element and append it to document body
-  const canvas = document.createElement('canvas');
+  const canvas = document.createElement("canvas");
 
-  canvas.setAttribute('id', 'user-image-canvas')
+  canvas.setAttribute("id", "user-image-canvas");
   div.appendChild(canvas);
 
   // some hotfixes... ( ≖_≖)
@@ -83,26 +81,26 @@ function createCanvas(div) {
   // canvas.style.position = 'fixed';
 
   // get canvas 2D context and set him correct size
-  const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext("2d");
   resize();
 
   // last known position
   const pos = { x: 0, y: 0 };
 
   // window.addEventListener('resize', resize);
-  canvas.addEventListener('mousemove', draw);
-  canvas.addEventListener('mousedown', setPosition);
-  canvas.addEventListener('mouseenter', setPosition);
+  canvas.addEventListener("mousemove", draw);
+  canvas.addEventListener("mousedown", setPosition);
+  canvas.addEventListener("mouseenter", setPosition);
 
   // new position from mouse event
   function setPosition(e) {
-    var X,Y;
+    var X, Y;
     X = e.pageX || e.clientX || e.offsetX;
     Y = e.pageY || e.clientY || e.offsetY;
     X = X - canvas.offsetLeft;
     Y = Y - canvas.offsetTop;
-    pos.x = X 
-    pos.y = Y
+    pos.x = X;
+    pos.y = Y;
   }
 
   // resize canvas
@@ -120,8 +118,8 @@ function createCanvas(div) {
     ctx.beginPath(); // begin
 
     ctx.lineWidth = 5;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = '#c0392b';
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "#c0392b";
 
     ctx.moveTo(pos.x, pos.y); // from
     setPosition(e);
@@ -131,5 +129,5 @@ function createCanvas(div) {
   }
 
   // pull random prompt and create/display element
-  displayPrompt(div)
+  displayPrompt(div);
 }
--- frontend/src/display-image.js
+++ frontend/src/display-image.js
@@ -1,84 +1,98 @@
 function displayImage() {
-  const drawingsUrl = "http://localhost:3000/drawings/"
-  const promptsUrl = "http://localhost:3000/prompts/"
-  const imageDiv = document.getElementById("game-content")
-  let score = 0
+  const container = document.getElementById("container");
+  container.className = "sp-guess";
+
+  const promptDiv = document.createElement("div");
+  promptDiv.id = "prompts";
+
+  const drawingsUrl = "http://localhost:3000/drawings/";
+  const promptsUrl = "http://localhost:3000/prompts/";
+  const contentDiv = document.getElementById("game-content");
+  let score = 0;
+  let imagesShown = 0;
 
   function fetchImage() {
     fetch(drawingsUrl)
-      .then(res => res.json())
-      .then(renderImage)
+      .then((res) => res.json())
+      .then(renderImage);
   }
 
   function renderImage(image) {
-    imageDiv.innerHTML = ''
+    contentDiv.innerHTML = "";
+    promptDiv.innerHTML = "";
 
-    const imageElement = document.createElement("img")
-    const randomImg = image[Math.floor(Math.random() * image.length)]
+    const imageElement = document.createElement("img");
+    const randomImg = image[Math.floor(Math.random() * image.length)];
 
-    imageElement.src = "assets/" + randomImg.file
+    imageElement.src = "assets/" + randomImg.file;
 
-    displayScore()
-    imageDiv.appendChild(imageElement)
+    displayScore();
+    contentDiv.appendChild(imageElement);
 
     // passing in the displayed image
-    renderPrompts(randomImg)
+    renderPrompts(randomImg);
   }
 
   function displayScore() {
-    const scoreElement = document.createElement("h1")
+    const scoreElement = document.createElement("h1");
     scoreElement.innerHTML = `
       <h1>score: <span id="score">${score}</span></h1>
-    `
+    `;
 
-    imageDiv.prepend(scoreElement)
+    promptDiv.prepend(scoreElement);
   }
 
   async function fetchPrompts() {
     // response awaits the completion of the fetch request before storing the return value
-    const response = await fetch(promptsUrl)
+    const response = await fetch(promptsUrl);
 
     // returns the json
-    return response.json()
+    return response.json();
   }
 
   async function renderPrompts(image) {
     // fetchPrompts returns a promise. the await keyword forces allPrompts to wait until the promise is resolved
-    let allPrompts = await fetchPrompts()  
-    const correctPrompt = createPromptElement(image, image.prompt)
+    let allPrompts = await fetchPrompts();
+    const correctPrompt = createPromptElement(image, image.prompt);
     // needs to be a set to prevent duplicate elements
-    const promptElementsSet = new Set()
-    promptElementsSet.add(correctPrompt)
+    const promptElementsSet = new Set();
+    promptElementsSet.add(correctPrompt);
 
     // selects 3 random prompts, creates an element, and adds it to the array alongside the correct prompt
-    while(promptElementsSet.size < 4) {
-      let randomPrompt = allPrompts[Math.floor(Math.random() * allPrompts.length)]
+    while (promptElementsSet.size < 4) {
+      let randomPrompt =
+        allPrompts[Math.floor(Math.random() * allPrompts.length)];
       if (randomPrompt !== correctPrompt) {
-        promptElementsSet.add(createPromptElement(image, randomPrompt))
+        promptElementsSet.add(createPromptElement(image, randomPrompt));
       }
     }
 
-    promptElementsSet.forEach(element => imageDiv.appendChild(element))
+    promptElementsSet.forEach((element) => promptDiv.appendChild(element));
+    container.appendChild(promptDiv);
   }
 
   function createPromptElement(image, prompt) {
-    const promptElement = document.createElement("h2")
-    promptElement.dataset.correct = image.prompt_id === prompt.id ? "true" : "false"
-    promptElement.dataset.action = "guess"
-    promptElement.innerHTML = prompt.title
+    const promptElement = document.createElement("h2");
+    promptElement.dataset.correct =
+      image.prompt_id === prompt.id ? "true" : "false";
+    promptElement.dataset.action = "guess";
+    promptElement.innerHTML = prompt.title;
 
-    return promptElement
+    return promptElement;
   }
-  
-  imageDiv.addEventListener("click", e => {
-    if (e.target.dataset.action === "guess") {
-      const scoreElement = document.getElementById("score")
-      if (e.target.dataset.correct === "true") {
-        scoreElement.innerHTML++
-        score = scoreElement.innerHTML
-      }
-    }
-  })
 
-  fetchImage()
+  promptDiv.addEventListener("click", (e) => {
+    if (e.target.dataset.action === "guess") {
+      const scoreElement = document.getElementById("score");
+      imagesShown++;
+      if (e.target.dataset.correct === "true") {
+        scoreElement.innerHTML++;
+        score = scoreElement.innerHTML;
+      }
+
+      fetchImage();
+    }
+  });
+
+  fetchImage();
 }
