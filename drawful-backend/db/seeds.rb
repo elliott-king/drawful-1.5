d759dac1f9ce74cb73c74c983c47750298d6d4e8
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-05-29 10:40:53 -0400

    Add prompts to seedfile

--- drawful-backend/db/seeds.rb
+++ drawful-backend/db/seeds.rb
@@ -8,13 +8,38 @@ Game.destroy_all
 g = Game.create!
 u = User.create!(username: "SeedUser", game: g)
 
-5.times do 
-  Prompt.create!(title: "#{Faker::DcComics.hero} #{Faker::Verb.ing_form} #{Faker::Space.planet}", user: u)
+prompt_strings = [
+  "magical musical cowboy",
+  "the sidewalk's end",
+  "beyonce with a flamethrower",
+  "a rocket powered by love",
+  "desperate house flies",
+  "sinful banjo",
+  "pupper in paradise",
+  "a helpful serving of bees",
+  "mahogany treasure chest filled with relics from my childhood",
+  "hipster batman",
+  "steve doran",
+  "a large man next to a smaller man",
+  "kinky boots",
+  "testing my sanity",
+  "optimus prime "
+]
 
-  Prompt.create!(title: "#{Faker::Verb.ing_form} #{Faker::House.furniture}", user: u)
-
-  Prompt.create!(title: "#{Faker::Creature::Animal.name} #{Faker::Creature::Animal.name}", user: u)
+prompt_strings.each do |prompt|
+  Prompt.create(title: prompt, user: u)
 end
+
+Prompt.create(title: "president duck", user: u)
+Prompt.create(title: "taco cat", user: u)
+Prompt.create(title: "corona virus", user: u)
+Prompt.create(title: "racing dresser", user: u)
+Prompt.create(title: "modern art", user: u)
+Prompt.create(title: "the pope", user: u)
+Prompt.create(title: "bald eagle wearing a hat", user: u)
+Prompt.create(title: "just a regular guy", user: u)
+Prompt.create(title: "poster for the goonies", user: u)
+Prompt.create(title: "extremely tight pants", user: u)
 
 Drawing.create!(file: "IMG_8201.JPG", user_id: u.id, game: g)
 Drawing.create!(file: "IMG_8203.JPG", user_id: u.id, game: g)
