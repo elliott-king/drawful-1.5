742d8d0d23ff970ef2be25032a0cd5ddd41c4a29
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-09-14 11:45:36 -0400

    Create activestorage for images

--- drawful-backend/db/schema.rb
+++ drawful-backend/db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_123204) do
+ActiveRecord::Schema.define(version: 2020_09_14_133354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "drawing_prompts", force: :cascade do |t|
     t.bigint "prompt_id"
@@ -26,7 +47,6 @@ ActiveRecord::Schema.define(version: 2020_05_28_123204) do
   end
 
   create_table "drawings", force: :cascade do |t|
-    t.string "file"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -65,6 +85,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_123204) do
     t.integer "game_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "drawings", "users"
   add_foreign_key "guesses", "drawings"
 end
